7c2022b87a2fde6cba6639096e602fbd549ee7fb
Administrator <Administrator@ZUOYAO>
2018-02-28 17:10:14 +0800

    ss

--- src/main/java/com/cn/core/util/TokenProccessor.java
+++ src/main/java/com/cn/core/util/TokenProccessor.java
@@ -6,7 +6,6 @@ package com.cn.core.util;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Random;
-import sun.misc.BASE64Encoder;
 
 /**
  * @author ZUO
@@ -38,11 +37,12 @@ public class TokenProccessor {
 			MessageDigest md = MessageDigest.getInstance("md5");
 			byte md5[] = md.digest(token.getBytes());
 			// base64编码--任意二进制编码明文字符 adfsdfsdfsf
-			BASE64Encoder encoder = new BASE64Encoder();
-			return encoder.encode(md5);
+			//BASE64Encoder encoder = new BASE64Encoder();
+			//return encoder.encode(md5);
 		} catch (NoSuchAlgorithmException e) {
 			throw new RuntimeException(e);
 		}
+		return null ;
 	}
 	
 	/*
